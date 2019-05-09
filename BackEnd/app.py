400eeddcb473a579e4ebb4d1bddc297ab1476fb1
Malik Albeik <abd.beik@me.com>
2019-05-09 23:08:50 +0300

    Fix: shortened the code

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -15,71 +15,10 @@ migrate = Migrate(app, db)
 from models import User, Event, EventSchema
 
 @app.route("/", methods=["GET", "POST"])
-def register():
+def events():
     if request.method == "GET":
-        allevents = Event.query.all()
-        response = jsonify(EventSchema(many=True).dump(allevents).data)
-        return response
+        return jsonify(EventSchema(many=True).dump(Event.query.all()).data)
 
-@app.route("/event/<int:eventId>", methods=["GET", "POST"])
+@app.route("/event/<int:eventId>", methods=["GET"])
 def event(eventId):
-    if request.method == "GET":
-        return jsonify(EventSchema(many=False).dump(Event.query.get(eventId)).data)
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user """
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # get password, username, name and surname from form.
-        password = request.args.get('hash')
-        username = request.args.get("username")
-        name = request.args.get("name")
-        surname = request.args.get("surname")
-        # Ensure username was submitted
-        if not username:
-            return 'Username is required.'
-
-        # Ensure password was submitted
-        if not password:
-            return 'Password is required.'
-        # Ensure name was submitted 
-        if not firstname:
-            return 'firstname is required.'
-        # Ensure surname was submitted
-        if not lastname:
-            return 'lastname is required.'
-        # Ensure username exists and password is correct
-        if error is None:
-            # myUser = User(username="dgjlskjf", hash="slkjfldfskl", firs )
-            db.execute(
-                'INSERT INTO user (username, password, name, surname) VALUES (?, ?, ?, ?)',
-            )
-            db.commit()
-            return True
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    """Log user in"""
-
-    # User reached route via POST (as by submitting a form via POST)
-    if request.method == "POST":
-
-        # get password and user from form.
-        password = request.args.get('hash')
-        username = request.args.get("username")
-
-        # Ensure username was submitted
-        if not username:
-            return 'Username is required.'
-
-        # Ensure password was submitted
-        if not password:
-            return 'Password is required.'
-
-        # Ensure username exists and password is correct
-        if not check_password_hash(user.hash, password):
-            return False
-    return True
+    return jsonify(EventSchema(many=False).dump(Event.query.get(eventId)).data)
