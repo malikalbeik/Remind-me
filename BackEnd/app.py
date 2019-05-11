a9cf78323ce59aad8a7db04ccc93b25bce60012c
Malik Albeik <abd.beik@me.com>
2019-05-11 18:29:19 +0300

    Add: filled the events.db database

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -8,6 +8,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'events.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 migrate = Migrate(app, db)
